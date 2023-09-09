1cb27a1244ca17e51d3eafe3b29da7b0e6803f84
abesari <abesari.aaron@gmail.com>
2023-09-09 15:17:50 -0400

    running react server and basic react app working

---
+++ client/src/app.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CategoryAdmin from './categoryAdmin';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <CategoryAdmin />
+    </div>
+  );
+};
+
+export default App;
---
+++ client/src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';  // Make sure the path to your App component is correct.
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
