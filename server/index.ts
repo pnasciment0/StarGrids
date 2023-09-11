5d2dcad87dbd85ed501b1bcf591a9da2c8702760
Paulo Nascimento <pnascimento@uchicago.edu>
2023-09-10 17:20:06 -0700

    port 8001

--- server/index.ts
+++ server/index.ts
@@ -9,7 +9,7 @@ console.log('DB_URL:', process.env.DB_URL);
 
 import app from './app';
 
-const PORT: string | number = process.env.PORT || 5000;
+const PORT: string | number = process.env.PORT || 8001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
